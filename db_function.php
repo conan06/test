ed7ae422c505eabbf316564b82f1468df0cb1e4f
Schwinn Hung <conan06@126.com>
2016-03-14 21:55:18 +0800

    Added files via upload

--- db_function.php
+++ db_function.php
@@ -37,16 +37,15 @@ class DB_Functions {
     }
 	
     /**
-     * Get data from config_ethernet table
+     * Get data from table
      */
-    public function getDataFromConfigEthernet() {
+    public function getData($query) {
         
         $mysqli = $this->conn;
         
         if($mysqli->connect_error) {
             die("$mysqli->connect_errno: $mysqli->connect_error");
         }
-        $query = "SELECT * FROM config_ethernet";
         $stmt = $mysqli->stmt_init();
         
         if(!$stmt->prepare($query)) {
@@ -63,6 +62,40 @@ class DB_Functions {
         } else {
             return NULL;
         }
+    }
+    
+    /**
+     * Get data from config_ethernet table
+     */
+    public function getDataFromConfigEthernet() {
+        
+        $query = "SELECT * FROM config_ethernet";
+        return self::getData($query);
+    }
+    
+    /**
+     * Get transfer data within 24 hours from monitor_history table
+     */
+    public function getWithinDayTransfer() {
+        
+        $query = "SELECT HOUR( historyTimeDone ) *60 *60 *1000 AS hours, COUNT( HOUR( historyTimeDone ) ) AS sum
+                FROM `monitor_history` 
+                WHERE DATE( historyTimeDone ) = CURDATE( ) 
+                GROUP BY HOUR( historyTimeDone ) ";
+        return self::getData($query);
+    }
+    
+    /**
+     * Get last week transfer data from monitor_history table
+     */
+    public function getLastWeekTransfer() {
+        
+        $query = "SELECT DATE( historyTimeDone ) AS days, COUNT( DATE( historyTimeDone ) ) AS sum
+                FROM `monitor_history` 
+                WHERE DATE_SUB( CURDATE( ) , INTERVAL 7 DAY ) < DATE( historyTimeDone ) 
+                GROUP BY DATE( historyTimeDone ) 
+                ORDER BY historyTimeDone DESC";
+        return self::getData($query);
     }
 }
 ?>
--- json_monitor_charts.php
+++ json_monitor_charts.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set('PRC'); // 设置时区为GMT+8
+
 require_once 'db_function.php';
 $db = new DB_Functions();
 
@@ -20,14 +22,14 @@ if (isset($input['type']) && $input['type'] != '') {
 	// 获取参数
     $type = $input['type'];
 	
-	if ($type == 'ethernet') {
-
-		// 从 config_ethernet 表中获取数据
-		$result = $db->getDataFromConfigEthernet();
+	if ($type == 'last24hrs') {
+		
+        $result = $db->getWithinDayTransfer();
 		
 		if ($result) {
             // 读取成功
             while ($row = $result->fetch_array(MYSQLI_NUM)) {
+                // $data[0] = strtotime($today) * 1000; // 将10位时间戳扩展成兼容JS的13位;
                 $response[] = $row;
             }
 			echo jsonRemoveUnicodeSequences($response);
@@ -38,10 +40,41 @@ if (isset($input['type']) && $input['type'] != '') {
             
 			echo jsonRemoveUnicodeSequences($response);
 		}
-	} else if ($type == 'hours') {
-		// 从 xx 表中获取数据
-        echo "[[0, 3.0], [3, 3.9], [6, 2.0], [9, 1.2], [12, 1.3], [15, 2.5], [18, 2.0], [21, 3.1]]";
-	}
+	} else if ($type == 'lastweek') {
+
+		$result = $db->getLastWeekTransfer();
+		
+		if ($result) {
+            // 读取成功
+            $today = date('Y-m-d');
+            $data = array();
+            $row = $result->fetch_array(MYSQLI_NUM);
+            
+            for ($i = 0; $i <= 6; $i++) {
+                $today = date('Y-m-d', strtotime('-'.$i.' day')); // 在today的基础上减一天
+                $data[0] = $today;
+                $data[0] = strtotime($today) * 1000;
+                
+                // if (!strcmp($row[0],$today)) {
+                if (strtotime($row[0]) * 1000 == $data[0]) {
+                    $data[1] = $row[1];
+                    $row = $result->fetch_array(MYSQLI_NUM);
+                } else {
+                    $data[1] = 0;
+                }
+                $response[] = $data;
+            }
+			echo jsonRemoveUnicodeSequences($response);
+		} else {
+			// 读取失败
+			$response["error"] = TRUE;
+			$response["error_msg"] = "数据读取失败";
+            
+			echo jsonRemoveUnicodeSequences($response);
+		}
+    } else if($type == 'thisyear') {
+        
+    }
 } else {
 		$response["error"] = TRUE;
 		$response["error_msg"] = "缺少参数";
